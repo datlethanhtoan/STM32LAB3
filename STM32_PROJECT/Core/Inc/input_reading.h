17126c4ee861e3d86cc7fe0c13973840bd133db5
ASUS\Administrator <dat.lethanhtoan@hcmut.edu.vn>
2023-11-09 18:55:57 +0700

    up

---
+++ STM32_PROJECT/Core/Inc/input_reading.h
@@ -0,0 +1,15 @@
+/*
+ * Input_processing.h
+ *
+ *  Created on: Nov 2, 2023
+ *      Author: HIEU
+ */
+
+#ifndef INC_INPUT_READING_H_
+#define INC_INPUT_READING_H_
+void button_reading(void);
+unsigned char is_button_pressed(unsigned char index);
+unsigned char is_button_pressed_1s(unsigned char index);
+
+
+#endif /* INC_INPUT_READING_H_ */
